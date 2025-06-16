9a97b5ea88070791c4d307cd57a5399f8ab79e22
LaipoSanni <ladicorp24m@gmail.com>
2025-06-16 14:14:53 +0100

    Initial commit - Blue-Green github deployment setup

--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"This is version: {os.getenv('APP_VERSION', 'unknown')}"
+    return f"This is version: {os.getenv('APP_VERSION', '1st version')}"
+
+@app.route("/health")
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
